4424ec5e5ce85dc3beba73b71278c760afd636f4
Boshell <johan.boshell@utp.edu.co>
2026-01-08 15:45:01 -0500

    refactor: Cambio en el AuthGuard para que escuche activamente los cambios en el accessToken Subject

--- application/src/guards/auth-guard.ts
+++ application/src/guards/auth-guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import { TokenService } from '../services/token/token.service';
+import { map, Observable, of, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,22 @@ export class AuthGuard implements CanActivate {
 
   constructor(private tokenService: TokenService, private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(): Observable<boolean> {
     if(this.tokenService.hasToken()){
-      return true;
+      return of(true);
     }
 
-    this.router.navigate(['/login']);
-    return false;
+    return this.tokenService.accessToken$.pipe(
+      take(1),
+      map(token => {
+        if(token){
+          return true;
+        }
+
+        //No hay token -> Redirigir a Login
+        this.router.navigate(['/login']);
+        return false;
+      })
+    )
   }
 }
