0722fc4a24aa9ea2b3d2071e5ec53f60a92eea89
Boshell <johan.boshell@utp.edu.co>
2026-01-14 14:28:42 -0500

    fix: Tipado de req.user

--- Backend/src/types/express.d.ts
+++ Backend/src/types/express.d.ts
@@ -1,9 +1,9 @@
-import { IUser } from '../modules/users/user.types';
+import { IUserAuth } from '../modules/users/user.types';
 
 declare global {
   namespace Express {
     interface Request {
-      user?: IUser;
+      user?: IUserAuth;
     }
   }
 }
