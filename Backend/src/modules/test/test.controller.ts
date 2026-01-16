912854e279f86759a262d5b9d5ca43a99a185bdc
Boshell <johan.boshell@utp.edu.co>
2026-01-15 23:57:39 -0500

    mig: Migración a ts de los routers

--- Backend/src/modules/test/test.controller.ts
+++ Backend/src/modules/test/test.controller.ts
@@ -1,13 +1,13 @@
+import {Request, Response} from 'express';
 import testService from './users.service.js';
 
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = async (req: Request, res: Response) => {
     try{
         const result = await testService.getAllUsers();
         return res.status(200).json({
             data: result,
         })
     } catch(err){
-        console.log("Error: ", err.message);
         return res.status(400).json({
             message: "Error consultando todos los usuarios"
         });
